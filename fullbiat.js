a47db5afe1de83f72314c283fe2b3fc4e94dca95
Floydy10 <140368482+Floydy10@users.noreply.github.com>
2023-07-26 11:57:12 +1000

    Update fullbiat.js

--- fullbiat.js
+++ fullbiat.js
@@ -44,9 +44,9 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor,Scorer, _) {
 			}, 
 			practiceCategory1 : 
 			{
-				name : 'Mammals', //Will appear in the data.
+				name : 'Others', //Will appear in the data.
 				title : {
-					media : {word : 'Mammals'}, //Name of the category presented in the task.
+					media : {word : 'Others'}, //Name of the category presented in the task.
 					css : {color:'#31b404','font-size':'1.8em'}, //Style of the category title.
 					height : 4, //Height (because we need to know where to put the next item in the title)
 					startStimulus : { 
