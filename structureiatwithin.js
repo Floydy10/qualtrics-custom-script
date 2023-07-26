610e7ada890615404b95cd39312ed99624bc4387
Floydy10 <140368482+Floydy10@users.noreply.github.com>
2023-07-26 11:55:55 +1000

    Update structureiatwithin.js

--- structureiatwithin.js
+++ structureiatwithin.js
@@ -5,9 +5,9 @@ define(['pipAPI', 'https://floydy10.github.io/qualtrics-custom-script/fullbiat.j
 	return iatExtension({
 		practiceCategory1 : 
 			{
-				name : 'Mammals', //Will appear in the data.
+				name : 'Others', //Will appear in the data.
 				title : {
-					media : {word : 'Mammals'}, //Name of the category presented in the task.
+					media : {word : 'Others'}, //Name of the category presented in the task.
 					css : {color:'#31b404','font-size':'1.8em'}, //Style of the category title.
 					height : 4, //Height (because we need to know where to put the next item in the title)
 					startStimulus : { 
