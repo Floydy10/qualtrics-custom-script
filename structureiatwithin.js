f60914e04c2147990dfa1170ed7dd23a29a87556
Floydy10 <140368482+Floydy10@users.noreply.github.com>
2023-07-26 16:13:07 +1000

    Update structureiatwithin.js

--- structureiatwithin.js
+++ structureiatwithin.js
@@ -57,7 +57,7 @@ define(['pipAPI', 'https://floydy10.github.io/qualtrics-custom-script/fullbiat.j
 						height : 4, //Height (because we need to know where to put the next item in the title)
 						startStimulus : { 
 						//If you're using a startStimulus, set here. If not, set the parameter showStimuliWithInst to false (see later below)
-						media : {image : 'white.jpg'}, 
+						media : {image : 'alb1thin.jpg'}, 
 							css : {color:'#31b404','font-size':'1em'}, 
 							height : 13
 						}
@@ -80,7 +80,7 @@ define(['pipAPI', 'https://floydy10.github.io/qualtrics-custom-script/fullbiat.j
 						css : {color:'#31b404','font-size':'1.8em'}, 
 						height : 4,
 						startStimulus : {
-							media : {image : 'white.jpg'},  
+							media : {image : 'alb1thin.jpg'},  
 							css : {color:'#31b404','font-size':'1em'}, 
 							height : 13
 						}
