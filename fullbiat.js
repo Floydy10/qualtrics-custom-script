774496e88bb6e6e8297987239b79f76e02ea9a82
Floydy10 <140368482+Floydy10@users.noreply.github.com>
2023-08-30 10:19:21 +1000

    Update fullbiat.js

--- fullbiat.js
+++ fullbiat.js
@@ -267,7 +267,7 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor,Scorer, _) {
 			instTemplate: '<div><p align="center" style="font-size:20px; font-family:arial"><br/>' +
 				'<font color="#000000"><u>Part blockNum of nBlocks</u><br/><br/></p>' + 
 				'<p style="font-size:20px; text-align:left; vertical-align:bottom; margin-left:10px; font-family:arial">' +
-				'You will be asked to do 9 matching tests. Each test will involve 20 trials. The first found is a practice round.<br/>' +
+				'You will be asked to do 9 matching tests. Each test will involve 20 trials.<br/>' +
 				'In each trial, you will be shown an item (see above).<br/>' +
 				'<div style="display: flex; justify-content: space-between;">' +
 					'<div style="flex: 1; background-color:#aaa; text-align: left">' +
